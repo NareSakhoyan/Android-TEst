fb864aa555de761c982b69d8e8c121a9400cd607
unknown <nare.sakhoyan@gmail.com>
2020-12-12 21:51:11 +0400

    added lifecycle methods and imageView

--- app/src/main/java/com/example/startup/MainActivity.java
+++ app/src/main/java/com/example/startup/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.startup;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ActionMode;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.EventListener;
@@ -18,6 +21,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private EditText password;
     private final String TAG = "This is my log: ";
     private Button myBtn;
+    private boolean loading = true;
+    private ImageView fruitImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +32,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         name = findViewById(R.id.name);
         password = findViewById(R.id.password);
         myBtn = findViewById(R.id.submit);
+        myBtn.setOnClickListener(this);
+
+        fruitImageView = findViewById(R.id.fruit);
+
+
 
         myBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -36,8 +46,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         myBtn.setOnClickListener(this);
 
-
         Log.d(TAG, getData());
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        this.loading = false;
+        Log.d(TAG, "Application is starting");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "application is onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "Application is onPause: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "Application is onStop: ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "application is onRestart: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "application is onDestroy: ");
     }
 
     public String getData() {
@@ -50,13 +96,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         if (v.equals(myBtn)) {
             Log.d(TAG, this.getData());
-            Intent intent = new Intent(this, NextActivity.class);
+            int id = getResources().getIdentifier("com.example.startup:drawable/way", null, null);
+            fruitImageView.setImageResource(id);
+
+//            Intent intent = new Intent(this, NextActivity.class);
 //            String value = "Nare1111111111";
 //            intent.putExtra("name", value);
 //            intent.putExtra("email", emailValue);
 //            intent.putExtra("phone", phoneValue);
-            startActivity(intent);
+//            startActivity(intent);
 //            connectDB.insert(name, password);
         }
     }
+
+
+
 }
